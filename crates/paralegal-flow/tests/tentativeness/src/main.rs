3e5a57bd5e9c870f3b6731d09ce5b39abb7776a5
MithiJ <mithi_jethwa@brown.edu>
2024-11-18 10:00:13 -0500

    tests for tentativeness

---
+++ crates/paralegal-flow/tests/tentativeness/src/main.rs
@@ -0,0 +1,101 @@
+struct User {
+    name: String,
+}
+
+#[paralegal::marker(user_data)]
+struct Image {
+    user: User,
+    name: String,
+}
+
+impl Image {
+    fn for_user(user: &User) {}
+    #[paralegal::marker(sink, return)] 
+    // This is not exactly correct but replicating ext annotations?
+    fn delete(&mut self) {}
+    fn modify(&mut self) {}
+}
+#[paralegal::marker(source, return)]
+fn get_from_database(database:&str, _id:&str) -> Image { 
+    Image {
+            user: User{
+                name: "dummy".to_string()
+            },
+            name: "dummy".to_string(),
+        }}
+
+const experimental_value: bool = false;
+
+#[paralegal::analyze]
+fn deletes_in_both_cases(img: &mut Image) {
+    if experimental_value  {
+        *img = get_from_database("database", img.name.as_str())
+    } else {
+        *img = Image {
+            user: User{
+                name: "dummy".to_string()
+            },
+            name: "dummy".to_string(),
+        }
+    }
+    img.delete()
+}
+// Key - Img.delete() overestimates a dependency on img from both branches
+// 
+
+#[paralegal::analyze]
+fn deletes_in_dummy_case(img: &mut Image) {
+    if experimental_value  {
+        *img = get_from_database("database", img.name.as_str())
+    } else {
+        *img = Image {
+            user: User{
+                name: "dummy".to_string()
+            },
+            name: "dummy".to_string(),
+        };
+        img.delete();
+    }
+}
+// Note: Here there is a case for policy refinement because if we say the image
+// from the database should - No tentativeness. 
+#[paralegal::analyze]
+fn conditional_modification(img: &mut Image) {
+    *img = get_from_database("database", img.name.as_str());
+    if experimental_value  {
+        *img = Image {
+            user: User{
+                name: "dummy".to_string()
+            },
+            name: "dummy".to_string(),
+        }
+    }
+    img.delete()
+}
+
+// We know that the loop never executes so the DB image does get deleted
+// Tentativeness comes from the fact that if the loop did execute (which we 
+// cannot know statically), then we would be sending a dummy value to the
+// delete sink as in the above cases.
+#[paralegal::analyze]
+fn modified_in_loop(img: &mut Image) {
+    *img = get_from_database("database", img.name.as_str());
+    for value in vec![] {
+        *img = Image {
+            user: User{
+                name: "dummy".to_string()
+            },
+            name: "dummy".to_string(),
+        }
+    }
+    img.delete();
+}
+
+#[paralegal::analyze]
+fn modifying_helper(img: &mut Image) {
+    *img = get_from_database("database", img.name.as_str());
+    *img = img.modify();
+    img.delete();
+}
+
+fn main() {}
---
+++ crates/paralegal-flow/tests/tentativeness_tests.rs
@@ -0,0 +1,69 @@
+#![feature(rustc_private)]
+#[macro_use]
+extern crate lazy_static;
+
+use paralegal_flow::test_utils::*;
+use paralegal_spdg::Identifier;
+
+const CRATE_DIR: &str = "tests/tentativeness";
+
+lazy_static! {
+    static ref TEST_CRATE_ANALYZED: bool = run_paralegal_flow_with_flow_graph_dump(CRATE_DIR);
+}
+
+macro_rules! define_test {
+    ($($t:tt)*) => {
+        paralegal_flow::define_flow_test_template!(TEST_CRATE_ANALYZED, CRATE_DIR, $($t)*);
+    };
+}
+
+// TODO: how do I get marked struct?
+// define_test!(deletes_in_both_cases: graph -> {
+//     // let user_data = Identifier::new_intern("user_data");
+//     let img = graph.marked(Identifier::new_intern("user_data"));
+//     let end = graph.marked(Identifier::new_intern("sink"));
+//     assert!(!end.is_empty());
+//     assert!(!img.is_empty());
+//     assert!(img.flows_to_any(&end));
+// });
+// The original version of Paralegal should pass this test since we have img 
+// connected to the img in the if-else and the one that passes into delete
+
+// Since the img.delete is only in one branch, there is no tentativeness
+// There is no edge from img.delete to the Image in the if-case
+define_test!(deletes_in_dummy_case: graph -> {
+    let source = graph.marked(Identifier::new_intern("source"));
+    let sink = graph.marked(Identifier::new_intern("sink"));
+    assert!(!source.is_empty());
+    assert!(!sink.is_empty());
+    assert!(!source.flows_to_any(&sink));
+});
+
+define_test!(conditional_modification: graph -> {
+    let source = graph.marked(Identifier::new_intern("source"));
+    let sink = graph.marked(Identifier::new_intern("sink"));
+    assert!(!source.is_empty());
+    assert!(!sink.is_empty());
+    assert!(source.flows_to_any(&sink));
+    // assert!(!source.flows_to_any(&sink));
+});
+// Here, source shouldn't flow to sink because there is tentativeness - if it 
+// enters the conditional branch, img is overwritten and doesn't get deleted.
+
+define_test!(modified_in_loop: graph -> {
+    let source = graph.marked(Identifier::new_intern("source"));
+    let sink = graph.marked(Identifier::new_intern("sink"));
+    assert!(!source.is_empty());
+    assert!(!sink.is_empty());
+    assert!(source.flows_to_any(&sink));
+    // assert!(!source.flows_to_any(&sink));
+});
+
+define_test!(modifying_helper: graph -> {
+    let source = graph.marked(Identifier::new_intern("source"));
+    let sink = graph.marked(Identifier::new_intern("sink"));
+    assert!(!source.is_empty());
+    assert!(!sink.is_empty());
+    assert!(source.flows_to_any(&sink));
+});
+
